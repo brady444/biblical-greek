f6a29f1eac6b5b74e75dddf0e6458803fd4c39bd
brady444 <16694009+brady444@users.noreply.github.com>
2023-09-04 14:55:05 -0700

    Update Vite config to automatically build and run the parser on changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { splitVendorChunkPlugin } from "vite";
 import eslint from "vite-plugin-eslint";
 import stylelint from "vite-plugin-stylelint";
 import replace from "vite-plugin-filter-replace";
+import { watch } from "vite-plugin-watch";
 
 export default {
 	root: "ui",
@@ -14,10 +15,18 @@ export default {
 		host: true
 	},
 	
+	json: {
+		stringify: true
+	},
+	
+	// Workaround to make Vite watch for changes in the parser directory, so that vite-watch can auto-run the parser on changes
+	envDir: "../parser",
+	
 	plugins: [
 		splitVendorChunkPlugin (),
 		eslint (),
 		stylelint (),
+		
 		replace.default ([
 			{
 				filter: /.js$/u,
@@ -27,6 +36,11 @@ export default {
 					to: ""
 				}
 			}
-		])
+		]),
+		
+		watch ({
+			pattern: "../parser/**/*",
+			command: "npm run parser"
+		})
 	]
 };
